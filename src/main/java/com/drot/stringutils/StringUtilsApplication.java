e784db6f665a4f011c696463d5291b56f2341e45
aleksandrdrot <aleksandr_drot@rambler.ru>
2023-05-03 16:14:08 +0300

    String Utils

---
+++ src/main/java/com/drot/stringutils/StringUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.drot.stringutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringUtilsApplication.class, args);
+	}
+
+}
